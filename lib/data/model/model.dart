358638a419fd5ae6d3b3d2824de6cfb70b6fc0ba
realapril <39916556+realapril@users.noreply.github.com>
2021-11-12 01:58:30 +0900

    Add enum and prototype models

---
+++ lib/data/model/model.dart
@@ -0,0 +1,17 @@
+
+class Coin{
+  var name;
+  var icon;
+  var symbol;
+  var price;
+}
+
+class Transaction{
+  var type;
+  var date;
+  var pricePerCoin;
+  var Quantity;
+  var fee;
+  var cost;
+  var note;
+}
